9fd8153a91293b276993f88ed1cf9d22877cfee6
Luz Maria <lu.maidacl@gmail.com>
2020-08-10 23:26:38 -0500

    Charts

--- src/views/fulfillment/Orders.vue
+++ src/views/fulfillment/Orders.vue
@@ -1,6 +1,29 @@
 <template>
   <v-container class="mt-9 mb-16">
     <v-row>
+      <v-col cols="12" sm="4">
+        <span class="ml-3 text-h6">Sessions</span>
+        <v-card>
+            <bar-chart :chart-data="datacollection"></bar-chart>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" sm="4">
+        <span class="ml-3 text-h6">Carts</span>
+        <v-card>
+          <line-chart :chart-data="cartcollection"></line-chart>
+        </v-card>
+      </v-col>
+
+      <v-col cols="12" sm="4">
+        <span class="ml-3 text-h6">Revenue</span>
+        <v-card>
+          <bar-chart :chart-data="revenuecollection"></bar-chart>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
       <v-col cols="12">
       <span class="ml-3 text-h6">All active orders</span>
       </v-col>
@@ -40,15 +63,24 @@
 <script>
 import { firestore } from "@/main"
 import AssignOrder from '@/components/fulfillment/AssignOrder.vue'
+import BarChart from "@/components/graphs/BarChart";
+import LineChart from "@/components/graphs/LineChart";
+import moment from "moment";
+
 export default {
   name: "InternalOrders",
   components: {
-    AssignOrder
+    AssignOrder,
+    BarChart,
+    LineChart
   },
   data: () => {
     return {
       dialog: false,
       selectedOrder: {},
+      datacollection: {},
+      revenuecollection: {},
+      cartcollection: {},
       headers: [
         {
           text: "Order Number",
@@ -81,6 +113,12 @@ export default {
       orders: [],
     };
   },
+  mounted () {
+    setTimeout(() =>{
+      this.fillData().bind(null,this);
+    },1500);
+
+  },
   methods: {
     closeDialog () {
       this.dialog = false
@@ -97,11 +135,57 @@ export default {
     manageOrder (order) {
       this.selectedOrder = order
       this.dialog = true
+    },
+    fillData () {
+      const labels = this.stats.map(element => moment(element.date.seconds*1000).format("DD/MM"));
+      const sessionDataset =[{
+        label: "User Sessions",
+        backgroundColor: '#4caf50',
+        data: this.stats.map(element => element.sessions)
+        }];
+
+      const cartDataset =[{
+          label: "Completed orders",
+          borderColor: '#f44336',
+          fill: false,
+          data: this.stats.map(element => element.completed_orders)
+        },
+        {
+          label: "Total orders",
+          borderColor: '#555555',
+          fill: false,
+          data: this.stats.map(element => element.total_orders)
+        }];
+
+      const revenueDataset =[{
+        label: "Revenue (Thousands $)",
+        backgroundColor: '#1565C0',
+        data: this.stats.map(element => element.amount/1000)
+      }];
+
+      this.datacollection = {
+        labels: labels,
+        datasets:sessionDataset
+      }
+
+      this.revenuecollection = {
+        labels: labels,
+        datasets:revenueDataset
+      }
+
+      this.cartcollection = {
+        labels: labels,
+        datasets:cartDataset
+      }
+    },
+    getRandomInt () {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
     }
   },
   firestore() {
     return {
       orders: firestore.collection("orders").orderBy('date').orderBy('time'),
+      stats: firestore.collection("stats").orderBy('date'),
     }
   },
 };
